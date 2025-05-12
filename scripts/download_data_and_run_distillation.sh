37dacaf8216daf154ff44651e63d2548ae807c63
Krasner <aakrasner@gmail.com>
2025-05-12 02:43:33 +0000

    Update paths

--- scripts/download_data_and_run_distillation.sh
+++ scripts/download_data_and_run_distillation.sh
@@ -2,7 +2,7 @@
 
 # download files
 # links can be found here: https://scontent-lga3-3.xx.fbcdn.net/m1/v/t6/An8MNcSV8eixKBYJ2kyw6sfPh-J9U4tH2BV7uPzibNa0pu4uHi6fyXdlbADVO4nfvsWpTwR8B0usCARHTz33cBQNrC0kWZsD1MbBWjw.txt?_nc_oc=AdgcU7b6EMb8dmnUOmBnNcphAQtGJcPBsftVcyHLozQOC7wsCRN9ahWKVnaCixV-YOaK5N1cpJrDD0kbn09POlFI&ccb=10-5&oh=00_AYGxM1hEmsynb7Lm6BLn996srLGL-JOKxT4c7VBLagRFuw&oe=67FDCA98&_nc_sid=0fdd51
-cd /home/ubuntu/EdgeSAM/datasets/
+cd /home/ubuntu/edgesam-dyt/datasets/
 mkdir train
 mkdir val
 
@@ -36,17 +36,17 @@ python extract_weights.py
 
 cd ..
 # pre-compute embeddings
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/EdgeSAM/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/save_embedding.py --cfg training/configs/teacher/sam_vit_huge_sa1b.yaml --batch-size 32 --eval --resume weights/sam_vit_h_4b8939.pth
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/edgesam-dyt/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/save_embedding.py --cfg training/configs/teacher/sam_vit_huge_sa1b.yaml --batch-size 32 --eval --resume weights/sam_vit_h_4b8939.pth
 
 # STEP 1: encoder distillation
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/EdgeSAM/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/train.py --cfg training/configs/rep_vit_m1_dyt_fuse_sa_distill.yaml --output ./output/ --batch-size 96
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/edgesam-dyt/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/train.py --cfg training/configs/rep_vit_m1_dyt_fuse_sa_distill.yaml --output ./output/ --batch-size 96
 
 # STEP 2: encoder + frozen mask decoder (same as in sam) distillation
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/EdgeSAM/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/train.py --cfg training/configs/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill_frozen_mask.yaml --output ./output/ --batch-size 16
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/edgesam-dyt/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/train.py --cfg training/configs/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill_frozen_mask.yaml --output ./output/ --batch-size 16
 
 # STEP 3: frozen encoder + mask decoder (layernorms replaced with dyt layers, gelu tanh approx) distillation
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/EdgeSAM/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/train.py --cfg training/configs/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill.yaml --output ./output/ --batch-size 16
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH="/home/ubuntu/edgesam-dyt/" python -m torch.distributed.launch --master_port 29501 --nproc_per_node 1 training/train.py --cfg training/configs/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill.yaml --output ./output/ --batch-size 16
 
 # export onnx
-PYTHONPATH="/home/ubuntu/EdgeSAM/" python scripts/export_onnx_model.py "/home/ubuntu/EdgeSAM/output/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill/default/ckpt_epoch_39.pth" --simplify
-PYTHONPATH="/home/ubuntu/EdgeSAM/" python scripts/export_onnx_model.py "/home/ubuntu/EdgeSAM/output/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill/default/ckpt_epoch_39.pth" --decoder --simplify --upsample
+PYTHONPATH="/home/ubuntu/edgesam-dyt/" python scripts/export_onnx_model.py "/home/ubuntu/edgesam-dyt/output/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill/default/ckpt_epoch_39.pth" --simplify
+PYTHONPATH="/home/ubuntu/edgesam-dyt/" python scripts/export_onnx_model.py "/home/ubuntu/edgesam-dyt/output/rep_vit_m1_dyt_fuse_enc_dec_4m_ft_bp_iter2b_sa_distill/default/ckpt_epoch_39.pth" --decoder --simplify --upsample
